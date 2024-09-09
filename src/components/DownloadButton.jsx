77221efa2c643c1b78560fe73f650f4cfa53d32a
nazarelis <anabelojeda2001@gmail.com>
2024-09-09 15:10:32 -0300

    cambios responsive al home

--- src/components/DownloadButton.jsx
+++ src/components/DownloadButton.jsx
@@ -5,7 +5,7 @@ function DownloadButton() {
     const { t } = useTranslation('home'); 
 
     return (
-        <a href="https://drive.google.com/uc?export=download&id=1aSM2vqfokbCoMaIYVkv9_YLP1RgZ1g5Y">
+        <a href="https://drive.google.com/uc?export=download&id=1aSM2vqfokbCoMaIYVkv9_YLP1RgZ1g5Y" className='pb-8 md:pb-0'>
 
         <div className="flex animate-slideIn animation-delay-1000 bg-[#F2BF5E] hover:bg-transparent hover:border-4 border-[#F2BF5E] rounded-3xl w-40 h-10 items-center text-raleway justify-center">
                 <button className="text-white">{t('button.downloadCV')}</button>
