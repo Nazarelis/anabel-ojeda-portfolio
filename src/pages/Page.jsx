fe06171eb026cf234903324287db86793ecfe5bd
nazarelis <anabelojeda2001@gmail.com>
2024-09-09 15:18:33 -0300

    cambios color del texto en navbar

--- src/pages/Page.jsx
+++ src/pages/Page.jsx
@@ -5,7 +5,7 @@ import WorkingImage from '../assets/Working.png'
 function Page()  {
     return(
         <div className="">
-        <Navbar textColor="text-black" bgColor="bg-[#F2BF5E]" hoverColor="hover:text-[#F2BF5E]" colorBurger="text-[#F2BF5E]" />
+        <Navbar textColor="text-black" bgColor="bg-[#F2BF5E]" hoverColor="hover:text-[#BF8450]" colorBurger="text-[#F2BF5E]" />
         
         <div className="flex items-center pt-24 justify-center w-full">
             <img className="w-2/6 " src={WorkingImage}></img>
