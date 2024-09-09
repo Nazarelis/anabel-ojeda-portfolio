98a3fac5f61070928e6afbf7a8caac13976280fe
nazarelis <anabelojeda2001@gmail.com>
2024-09-09 15:02:03 -0300

    cambios en background del navbar para menu mobile

--- src/pages/Page.jsx
+++ src/pages/Page.jsx
@@ -1,9 +1,11 @@
 import Navbar from "../components/Navbar"
+
 import WorkingImage from '../assets/Working.png'
+
 function Page()  {
     return(
         <div className="">
-        <Navbar textColor="text-black" bgColor="bg-transparent" hoverColor="hover:text-[#F2BF5E]" colorBurger="text-white" />
+        <Navbar textColor="text-black" bgColor="bg-[#F2BF5E]" hoverColor="hover:text-[#F2BF5E]" colorBurger="text-[#F2BF5E]" />
         
         <div className="flex items-center pt-24 justify-center w-full">
             <img className="w-2/6 " src={WorkingImage}></img>
