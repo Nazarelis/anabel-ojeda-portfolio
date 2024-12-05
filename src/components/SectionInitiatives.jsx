2f6bf70342dc7b6e067ee347b0017899cac49839
nazarelis <anabelojeda2001@gmail.com>
2024-12-05 11:11:29 -0300

    cambios en las imagenes, ahora importadas como módulos

--- src/components/SectionInitiatives.jsx
+++ src/components/SectionInitiatives.jsx
@@ -1,41 +1,43 @@
-// import Button from './Button'
+// Importamos las imágenes como módulos
+import dulceC from '../assets/dulcec.png';
+import fondoIncognita from '../assets/fondoincognita.jpeg';
+
 import { useTranslation } from 'react-i18next';
 import CardWeb from './CardWebs';
-import {motion} from 'framer-motion'
+import { motion } from 'framer-motion';
 
-function SectionInitiatives(){
+function SectionInitiatives() {
     const { t } = useTranslation('home');
 
-    return(
+    return (
         <>
-        <motion.div 
-        className='flex flex-col pt-10 items-center justify-center md:h-screen w-full bg-[#F2BF5E]'
-        >
-            <div className='flex flex-col justify-center items-center'>
-
-                <h2 className='text-[#01405B] font-raleway font-semibold mt-4 md:text-3xl'>
-                {t('titleWebsQC')}
-                </h2>
-                <p className='text-[#272F40] text-center font-robotoSlab md:text-xl'>
-                {t('parrWebsQC')}
-                </p>
-            </div>
             <motion.div 
-            className='flex flex-col md:flex-row w-full justify-center '>
-                <CardWeb titleWebCard={t('titleCardOne')} pWebCard={t('parrCardOne')} src="src\assets\dulcec.png"/>
-                <CardWeb titleWebCard={t('titleCardTwo')} pWebCard={t('parrCardTwo')} src="src\assets\fondoincognita.jpeg"/>
-                <CardWeb titleWebCard={t('titleCardThree')} pWebCard={t('parrCardThree')} src="src\assets\fondoincognita.jpeg"/>
+                className="flex flex-col pt-10 items-center justify-center md:h-screen w-full bg-[#F2BF5E]"
+            >
+                <div className="flex flex-col justify-center items-center">
+                    <h2 className="text-[#01405B] font-raleway font-semibold mt-4 md:text-3xl">
+                        {t('titleWebsQC')}
+                    </h2>
+                    <p className="text-[#272F40] text-center font-robotoSlab md:text-xl">
+                        {t('parrWebsQC')}
+                    </p>
+                </div>
+                <motion.div 
+                    className="flex flex-col md:flex-row w-full justify-center"
+                >
+                    {/* Usamos las imágenes importadas */}
+                    <CardWeb titleWebCard={t('titleCardOne')} pWebCard={t('parrCardOne')} src={dulceC} />
+                    <CardWeb titleWebCard={t('titleCardTwo')} pWebCard={t('parrCardTwo')} src={fondoIncognita} />
+                    <CardWeb titleWebCard={t('titleCardThree')} pWebCard={t('parrCardThree')} src={fondoIncognita} />
+                </motion.div>
+                <div className="flex justify-center items-center w-auto">
+                    <h1 className="text-[#01405B] md:text-4xl font-raleway font-semibold mb-4">
+                        {t('comingtext')}
+                    </h1>
+                </div>
             </motion.div>
-            <div className='flex justify-center items-center w-auto'>
-            <h1 className='text-[#01405B] md:text-4xl font-raleway font-semibold mb-4'>
-                    {t('comingtext')}
-                </h1>
-            </div>
-
-        </motion.div>
-
         </>
-    )
+    );
 }
 
-export default SectionInitiatives
+export default SectionInitiatives;
--- src/components/SectionProjects.jsx
+++ src/components/SectionProjects.jsx
@@ -1,29 +1,32 @@
-// import ColorImage from '.'
-// import Button from './Button'
+import captureLakeBarrine from '../assets/capture-lakebarrine.jpeg';
+import captureAppTodo from '../assets/capture-apptodo.jpeg';
+import captureChichaStyle from '../assets/capture-chichastyle.jpeg';
+import captureNasa from '../assets/capture-Nasa.jpeg';
+
 import { useTranslation } from 'react-i18next';
 import CardProject from './CardProject';
 
-function SectionProjects(){
+function SectionProjects() {
     const { t } = useTranslation('home');
 
-    return(
+    return (
         <>
-        <div className="w-full h-auto bg-[#E9E9E9] flex justify-center flex-col">
-            <div className="flex justify-center items-center w-full md:mt-16">
-            <h1 className="flex flex-col text-[#F2BF5E] text-center text-5xl ">
-            {t('sectionFeaturedWorks')}
-            </h1>
+            <div className="w-full h-auto bg-[#E9E9E9] flex justify-center flex-col">
+                <div className="flex justify-center items-center w-full md:mt-16">
+                    <h1 className="flex flex-col text-[#F2BF5E] text-center text-5xl ">
+                        {t('sectionFeaturedWorks')}
+                    </h1>
+                </div>
+                <div className="w-full grid md:grid-cols-2 grid-cols-1 md:p-24 md:w-full">
+                    
+                    <CardProject src={captureLakeBarrine} href="https://lakebarrine.com.au/" />
+                    <CardProject src={captureAppTodo} href="https://nazarelis.github.io/TODOS_APP/" />
+                    <CardProject src={captureChichaStyle} href="https://chicha-style-foodtruck.web.app/" />
+                    <CardProject src={captureNasa} href="https://cosmicimage-1e517.web.app/" />
+                </div>
             </div>
-            <div className='w-full grid md:grid-cols-2 grid-cols-1 md:p-24 md:w-full'>
-                <CardProject src="src\assets\capture-lakebarrine.jpeg" href="https://lakebarrine.com.au/"/>
-                <CardProject src="src\assets\capture-apptodo.jpeg" href="https://nazarelis.github.io/TODOS_APP/"/>
-                <CardProject src="src\assets\capture-chichastyle.jpeg" href="https://chicha-style-foodtruck.web.app/"/>
-                <CardProject src="src\assets\capture-Nasa.jpeg" href="https://cosmicimage-1e517.web.app/"/>
-            </div>
-        </div>
-
         </>
-    )
+    );
 }
 
 export default SectionProjects;
